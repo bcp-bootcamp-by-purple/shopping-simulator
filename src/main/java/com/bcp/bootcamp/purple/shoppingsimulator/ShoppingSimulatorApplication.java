2d29d5e4a04e20817a6410d7230062cc71535fbc
Hector Hugo Ccasani <hccasani.hc@gmail.com>
2021-04-02 09:11:50 -0500

    project base

---
+++ src/main/java/com/bcp/bootcamp/purple/shoppingsimulator/ShoppingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.bcp.bootcamp.purple.shoppingsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingSimulatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingSimulatorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bcp/bootcamp/purple/shoppingsimulator/ShoppingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp.bootcamp.purple.shoppingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSimulatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
